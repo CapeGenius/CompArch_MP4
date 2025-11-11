5444b78a3734e080f874d41f25c95ee5d1cf72ee
Rixhee <rishitbansals@gmail.com>
2025-11-11 16:46:43 -0500

    implemented flip flops w/wo enable

---
+++ flop.sv
@@ -0,0 +1,18 @@
+module flop #(parameter WIDTH = 32)
+    (
+        input logic clk,
+        input logic reset,
+        input logic [WIDTH - 1: 0] data,
+        output logic [WIDTH - 1: 0] stored_value
+    );
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            stored_value <= 0;
+        end
+        else begin
+            stored_value <= data;
+        end
+    end
+
+endmodule
---
+++ flop_enable.sv
@@ -0,0 +1,19 @@
+module flop #(parameter WIDTH = 32)
+    (
+        input logic clk,
+        input logic reset,
+        input logic enable,
+        input logic [WIDTH - 1: 0] data,
+        output logic [WIDTH - 1: 0] stored_value
+    );
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            stored_value <= 0;
+        end
+        else if (enable) begin
+            stored_value <= data;
+        end
+    end
+
+endmodule
