55859ff630b70d81069bbe9b84c574cbc26d24e0
Rohan Bendapudi <datanteater@gmail.com>
2025-11-24 14:00:23 -0500

    finished adding read data flop and instruction register

--- datapath.sv
+++ datapath.sv
@@ -9,23 +9,20 @@
 `include "mux3.sv"
 
 module datapath (
-    input logic clk,
-    input logic adr_src, mem_write, IR_write, reg_write,
-    input logic [1:0] result_src, alu_src_a, alu_src_b, imm_src,
-    input logic [2:0] alu_control,
-    output logic [6:0] op_code,
-    output logic [2:0] funct3,
-    output logic [6:0] funct7, 
-    output logic Zero, 
-    input logic RGB_R, RGB_B, RGB_R
-);
+                input logic clk,
+                input logic adr_src, mem_write, IR_write, reg_write, PC_write,
+                input logic [1:0] result_src, alu_src_a, alu_src_b, imm_src,
+                input logic [2:0] alu_control,
+                output logic [6:0] op_code,
+                output logic [2:0] funct3,
+                output logic [6:0] funct7, 
+                output logic Zero, 
+                input logic RGB_R, RGB_B, RGB_R);
+    // create reset
+    logic gen_reset = 0; 
+    
     //create LED
     logic [31:0] led;
-
-    // create CU logic
-    logic PC_write_enable;
-    logic mem_write_signal;
-    logic adr_src;
 
     // create PC logic 
     logic PC_reset;
@@ -36,10 +33,15 @@ module datapath (
     logic [31:0] result;
 
     // declare instruction logic
-    logic [31:0] instruction;
+    logic [31:0] instruction_in, instruction_out;
 
     // declare data from data memory
     logic [31:0] dmem_out;
+
+    // declare logic for register
+    logic[4:0] rs1, rs2, rd1; // address of RD1
+    logic [31:0] write_data_input, read_data_1, read_data_2;
+    logic [31:0] stored_read_data_1, stored_read_data_2;
 
     //declaring all modules
 
@@ -48,9 +50,26 @@ module datapath (
     program_counter (
         .clk                (clk),
         .reset              (PC_reset),
-        .enable             (PC_write_enable),
+        .enable             (PC_write),
         .data               (PC_next),
         .stored_value       (PC_current)
+    );
+
+    //IR register
+    flop_enable #(.WIDTH (32)) 
+    instr_reg (
+        .clk                (clk),
+        .reset              (PC_reset),
+        .enable             (IR_write),
+        .data               (instruction_in),
+        .stored_value       (instruction_out)
+    );
+
+    flop main_reg_flop (
+        .clk            (clk),
+        .reset          (gen_reset),
+        .data           (read_data_1),
+        .stored_value   (stored_read_data_1)
     );
 
     // mux for address
@@ -87,8 +106,26 @@ module datapath (
     instruction_memory (
         .clk            (clk),
         .imem_address   (mem_adress),
-        .imem_data_out  (instruction)
+        .imem_data_out  (instruction_in)
     );
+
+    //main register
+    register main_register(
+        .clk                (clk), 
+        .write_enable_flag  (reg_write), 
+        .a1                 (rs1),
+        .a2                 (rs2),
+        .a3                 (rd1),
+        .write_data_input   (write_data_input),
+        .read_data_1        (read_data_1),
+        .read_data_2        (read_data_2).
+    );
+
+    extend  extend_immediate(
+        imm
+    )
+
+
 
 
 endmodule
