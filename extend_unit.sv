15d67b23854ed35378555bc9f4660aab3d337a99
Rohan Bendapudi <datanteater@gmail.com>
2025-11-27 20:12:35 -0500

    fixed bug with extend, supposed to receive 2 bits not 3 bits for immsrc

--- extend_unit.sv
+++ extend_unit.sv
@@ -1,20 +1,18 @@
 module extend(input logic [31:7] instr, 
-              input logic [2:0] immsrc, 
+              input logic [1:0] immsrc, 
               output logic [31:0] immext);
 
     always_comb begin
         case(immsrc)
             // I-type (Immediate)
-            3'b000: immext = {{20{instr[31]}}, instr[31:20]};
+            2'b00: immext = {{20{instr[31]}}, instr[31:20]};
             // S-type (Stores)
-            3'b001: immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+            2'b01: immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
             // B-type (Branch)
-            3'b010: immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-            // U-type (Upper)
-            3'b011: immext = {instr[31:12], 12'b0};
+            2'b10: immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+            // Jump
+            2'b11: immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
             // J-type (Jump)
-            3'b100: immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-            // Default (undefined)
             default: immext = 32'bx;
         endcase
     end
